3d07b98b89288d77d333189838868ad4b8576e2e
Aine O'Connor <22124@burnside.school.nz>
2024-09-27 17:04:19 +1200

    .

--- scripts/lever.gd
+++ scripts/lever.gd
@@ -4,5 +4,6 @@ signal leverpulled
 
 func _on_body_entered(body):
 	if body.name == "player":
-		#lever has been pulled, emit signal
+		#lever has been pulled, emit signal and change sprite
+		$AnimatedSprite2D.set_animation("R-pull")
 		leverpulled.emit()
